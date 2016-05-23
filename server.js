3d861a2fd658ab5eeadadffba5fb8175164d77b0
Mark Tse <mark.tse@neverendingqs.com>
2016-05-22 20:43:47 -0400

    Adding route to provide natural and unix times.

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var querystring = require('querystring');
 
+var timestamp = require('./lib/timestamp');
+
 var app = express();
 var port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
+});
+
+app.get('/:dtInput', function(req, res) {
+  res.json(timestamp.parseDt(req.params.dtInput));
 });
 
 // Must be last route to be registered
