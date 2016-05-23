03a6ed0b9cf26298b240b1ae3b989652ed23c592
Mark Tse <mark.tse@neverendingqs.com>
2016-05-23 18:38:28 -0400

    Added link to github on homepage

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
+app.get('/', function (req, res) {
+  res.send('Hello! More details about this site at <a href="https://github.com/neverendingqs-sandbox/nodejs-sampler">https://github.com/neverendingqs-sandbox/nodejs-sampler</a>.')
+});
+
 app.use('/timestamp', require('./routes/timestamp'));
 
 // Must be last route to be registered
